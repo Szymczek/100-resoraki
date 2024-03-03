86b32044d6325711ddfe07c2808d51d2bfe75898
Szymczek <szymon.szymczewski@protonmail.com>
2024-03-03 02:43:24 +0100

    Move to react and fiber

---
+++ 100-resoraki/src/App.js
@@ -0,0 +1,32 @@
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
+
+function Box() {
+  return (
+    <mesh>
+      <boxBufferGeometry args={[0.5, 1, 1]} />
+      <meshStandardMaterial color={"orange"} />
+    </mesh>
+  );
+}
+
+// function Sphere() {
+//   return (
+//     <mesh>
+//       <sphereBufferGeometry args={[1, 30, 30]} />
+//       <meshStandardMaterial color={"green"} />
+//     </mesh>
+//   );
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <Canvas>{<Box />}</Canvas>
+      </div>
+    </div>
+  );
+}
+
+export default App;
