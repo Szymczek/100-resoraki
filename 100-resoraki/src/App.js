9749490a642c8f6b81c81f376ad205e6770e9f30
Szymczek <szymon.szymczewski@protonmail.com>
2024-03-06 01:54:27 +0100

    movement

--- 100-resoraki/src/App.js
+++ 100-resoraki/src/App.js
@@ -1,39 +1,21 @@
-import { Canvas } from "@react-three/fiber";
-import "./App.css";
-
-function Box() {
-  return (
-    <mesh>
-      <boxBufferGeometry args={[0.5, 1, 1]} />
-      <meshStandardMaterial color={"orange"} />
-    </mesh>
-  );
-}
-
-function Sphere() {
-  return (
-    <mesh>
-      <sphereBufferGeometry args={[1, 30, 30]} />
-      <meshStandardMaterial color={"blue"} />
-    </mesh>
-  );
-}
-
+import React from "react";
+import ThreeDObject from "./ThreeDObject";
+import ReactDOM from "react-dom";
+// import ReactDOM from "@react-three/drei";
 function App() {
   return (
-    <div className="App">
-      <div className="App">
-        <Canvas>
-          {
-            <>
-              <Box />
-              <Box />
-            </>
-          }
-        </Canvas>
-      </div>
+    <div>
+      <h1>Witaj w mojej aplikacji React z Three.js!</h1>
+      <ThreeDObject />
     </div>
   );
 }
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 export default App;
---
+++ 100-resoraki/src/ThreeDObject.js
@@ -0,0 +1,47 @@
+import React, { useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Box } from "@react-three/drei";
+
+function ThreeDObject() {
+  const boxRef = useRef();
+
+  const handleKeyDown = (event) => {
+    const speed = 0.1;
+    const box = boxRef.current;
+
+    switch (event.key) {
+      case "w":
+        box.position.z -= speed;
+        break;
+      case "a":
+        box.position.x -= speed;
+        break;
+      case "s":
+        box.position.z += speed;
+        break;
+      case "d":
+        box.position.x += speed;
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <Canvas onKeyDown={handleKeyDown} style={{ outline: "none" }} tabIndex={0}>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box ref={boxRef}>
+        <meshStandardMaterial
+          attach="material"
+          color="#FFFFFF"
+          roughness={0.5}
+          metalness={0.5}
+          gradientMap={[0x000000, 0xffffff]} // Dodajemy gradientMap, aby uzyskać gradient
+        />
+      </Box>
+    </Canvas>
+  );
+}
+
+export default ThreeDObject;
