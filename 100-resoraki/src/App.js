6ca2ec377fea6abefd11b22b99c342d1317e46b9
Szymczek <szymon.szymczewski@protonmail.com>
2024-03-13 17:58:22 +0100

    add box

--- 100-resoraki/src/App.js
+++ 100-resoraki/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Scene from "./Scene";
-
+import { Box } from "@react-three/drei";
 function App() {
   return (
     <div>
       <h1>Witaj w mojej aplikacji React z Three.js!</h1>
       <Scene />
+      <Box />
       <p>End</p>
     </div>
   );
