26d7c8a6e7dad1a752eb44898ecbeea1395655db
Szymczek <szymon.szymczewski@capgemini.com>
2024-03-21 20:44:59 +0100

    frontend clean

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,13 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  title = 'frontend';
 }
--- frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-import packageInfo from 'package.json';
-
-export const environment = {
-  production: true,
-  version: packageInfo.version
-};
